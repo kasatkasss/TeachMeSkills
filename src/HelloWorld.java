9b8f94372ebee8d399aba159d6b5929761d59eba
kasatkasss <lysyaloslos@gmail.com>
2021-05-28 11:41:33 +0300

    firstCommit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 2.2;
+        a = (int)b;
+        System.out.println(a);
+
+
+    }
+}
