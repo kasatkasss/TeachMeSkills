2f946909e1eafef8e0f2c268e64549cae577434a
kasatkasss <lysyaloslos@gmail.com>
2021-05-28 11:21:20 +0300

    first

---
+++ src/Unit1Task4.java
@@ -0,0 +1,17 @@
+public class Unit1Task4 {
+    public static void main(String[] args) {
+        int count = 0;
+        int a = 5;
+        int b = 0;
+        int c = -11;
+        if(a > 0)
+            count++;
+        if(b > 0)
+            count++;
+        if(c > 0)
+            count++;
+        System.out.println("Количество положительных чисел: " + count);
+
+    }
+
+}
